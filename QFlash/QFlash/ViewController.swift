478de3f9dc159032318e82466101167932e8b47c
RyanNavillus <ryanpnavillus@gmail.com>
2019-03-25 18:30:50 -0400

    Initial Commit

---
+++ QFlash/QFlash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QFlash
+//
+//  Created by Ryan Sullivan on 3/25/19.
+//  Copyright © 2019 QFlash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
